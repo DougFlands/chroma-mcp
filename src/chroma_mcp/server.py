335fdc0d35b082895f1d11801c91524706513872
成钊希 <flands.cheng@choiceform.com>
2025-06-10 03:00:53 +0800

    test

--- src/chroma_mcp/server.py
+++ src/chroma_mcp/server.py
@@ -25,7 +25,7 @@ from chromadb.utils.embedding_functions import (
 
 # Initialize FastMCP server
 # mcp = FastMCP("chroma", settings={"log_level": "debug", "port": 8080})
-mcp = FastMCP("chroma", port= 8080,log_level="debug")
+mcp = FastMCP("chroma", port= 8080,log_level="DEBUG")
 
 # Global variables
 _chroma_client = None
